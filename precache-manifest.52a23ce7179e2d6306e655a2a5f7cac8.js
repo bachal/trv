d1fbd71447c108398d587ce29226cabb565f25df
somnathbachal023@gmail.com <somnathbachal023@gmail.com>
2020-09-07 19:32:24 +0530

    Updates

---
+++ precache-manifest.52a23ce7179e2d6306e655a2a5f7cac8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cb3ec4c6171339c1be585879763531f",
+    "url": "/trv/index.html"
+  },
+  {
+    "revision": "0f432026e3a56aa7d498",
+    "url": "/trv/static/css/2.db6f3515.chunk.css"
+  },
+  {
+    "revision": "2140b781b18d7a801534",
+    "url": "/trv/static/css/main.6c8554a5.chunk.css"
+  },
+  {
+    "revision": "0f432026e3a56aa7d498",
+    "url": "/trv/static/js/2.a08466ba.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/trv/static/js/2.a08466ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2140b781b18d7a801534",
+    "url": "/trv/static/js/main.c6f8779c.chunk.js"
+  },
+  {
+    "revision": "8b3d5017ab96f3588bb6",
+    "url": "/trv/static/js/runtime-main.246476d3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/trv/static/media/logo.5d5d9eef.svg"
+  }
+]);
